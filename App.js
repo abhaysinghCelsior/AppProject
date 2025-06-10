5d32eef4873e0f7eb913dd64ed197fd99a62c658
Abhay Kumar Singh <abhays@pyramidconsultinginc.com>
2025-06-10 17:59:21 +0530

    Initial

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Abhay - How are you!</Text>
+      <Text>This is time to build mobile app</Text>
       <StatusBar style="auto" />
     </View>
   );
